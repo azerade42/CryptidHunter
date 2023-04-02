377a984e50ce0f4f079561a310023506a1a9fac0
slapstickers <maxgeiringer@knights.ucf.edu>
2023-04-02 09:56:25 -0400

    Completed NightVision + Battery
    Mostly completed and functional NightVision and battery, just need to add small adjustments and maybe visual tweaks

--- Assets/Max/MaxAssets/Scripts/BatteryPercent.cs
+++ Assets/Max/MaxAssets/Scripts/BatteryPercent.cs
@@ -6,9 +6,27 @@ using UnityEngine.UI;
 public class BatteryPercent : MonoBehaviour
 {
     public Slider slider;
+    public GameObject batteryIcon;
+
+    void Start()
+    {
+        batteryIcon.SetActive(false);
+    }
 
     public void SetBatteryPercentage(float batPercent)
     {
         slider.value = batPercent;
+    }
+
+    public void BatteryVisible(bool batVis)
+    {
+        if(batVis == true)
+        {
+            batteryIcon.SetActive(true);
+        }
+        else
+        {
+            batteryIcon.SetActive(false);
+        }
     }
 }
